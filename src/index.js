2c546e5fef605c0d112eeb2b256f6af4a8067e28
Sheldon Kuo <nkeyskuo124@gmail.com>
2022-10-29 00:09:40 +0700

    change port

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 9090;
+const PORT = process.env.PORT || 3000;
 const db = require('./config/database');
 const router = require('./resources/routes');
 
@@ -7,6 +7,10 @@ const app = require('./config/server').init();
 db.connect();
 
 router(app);
+
+app.get('/home', (req, res, next) => {
+    res.render('pages/home/home');
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
