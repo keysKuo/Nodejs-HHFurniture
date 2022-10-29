6180af5866b834df00a590df6c637e502767b002
vinhnguyen-22 <vinhnguyenad22@gmail.com>
2022-10-29 23:36:47 +0700

    Homepage gần complete

---
+++ src/public/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function ($) {
+    $('.owl-carousel').owlCarousel({
+        loop: false,
+        margin: 10,
+        dots: false,
+        nav: true,
+        items: 5,
+    });
+    var owl = $('.owl-carousel');
+    owl.owlCarousel();
+    $('.next-btn').click(function () {
+        owl.trigger('next.owl.carousel');
+    });
+    $('.prev-btn').click(function () {
+        owl.trigger('prev.owl.carousel');
+    });
+    $('.prev-btn').addClass('disabled');
+    $(owl).on('translated.owl.carousel', function (event) {
+        if ($('.owl-prev').hasClass('disabled')) {
+            $('.prev-btn').addClass('disabled');
+        } else {
+            $('.prev-btn').removeClass('disabled');
+        }
+        if ($('.owl-next').hasClass('disabled')) {
+            $('.next-btn').addClass('disabled');
+        } else {
+            $('.next-btn').removeClass('disabled');
+        }
+    });
+    var mySwiper = new Swiper('.swiper-container', {
+        // Các Parameters
+        direction: 'horizontal',
+        loop: true,
+
+        // Nếu cần pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Nếu cần navigation
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // Nếu cần scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+    var mySwiper2 = new Swiper('.swiper-container-doitac', {
+        slidesPerView: 8,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        autoplay: 5000,
+        speed: 800,
+        autoplayDisableOnInteraction: false,
+    });
+});
--- src/resources/routes/Home.js
+++ src/resources/routes/Home.js
@@ -52,8 +52,103 @@ var lsCat = [
     },
 ];
 
+var lsProduct = [
+    {
+        title: 'Bàn Ăn Thông Minh Mahal Kita Home LTX-801M.13.CTD',
+        img: 'HH-FURNITURE_N.LTX-801M.13.CTD_1900866816-300x300.jpg',
+        href: '/',
+    },
+    {
+        title: 'Bàn Ăn Thông Minh Mahal Kita Home LTX-801M.13.CTD',
+        img: 'HH-FURNITURE_N.RIH-B19.CTV_1900866816-300x300.jpg',
+        href: '/',
+    },
+    {
+        title: 'Bàn Ăn Thông Minh Mahal Kita Home LTX-801M.13.CTD',
+        img: 'HH-FURNITURE_N.LTX-801M.13.CTD_1900866816-300x300.jpg',
+        href: '/',
+    },
+    {
+        title: 'Bàn Ăn Thông Minh Mahal Kita Home LTX-801M.13.CTD',
+        img: 'HH-FURNITURE_N.RIH-B19.CTV_1900866816-300x300.jpg',
+        href: '/',
+    },
+    {
+        title: 'Bàn Ăn Thông Minh Mahal Kita Home LTX-801M.13.CTD',
+        img: 'HH-FURNITURE_N.LTX-801M.13.CTD_1900866816-300x300.jpg',
+        href: '/',
+    },
+    {
+        title: 'Bàn Ăn Thông Minh Mahal Kita Home LTX-801M.13.CTD',
+        img: 'HH-FURNITURE_N.LTX-801M.13.CTD_1900866816-300x300.jpg',
+        href: '/',
+    },
+    {
+        title: 'Bàn Ăn Thông Minh Mahal Kita Home LTX-801M.13.CTD',
+        img: 'HH-FURNITURE_N.LTX-801M.13.CTD_1900866816-300x300.jpg',
+        href: '/',
+    },
+    {
+        title: 'Bàn Ăn Thông Minh Mahal Kita Home LTX-801M.13.CTD',
+        img: 'HH-FURNITURE_N.LTX-801M.13.CTD_1900866816-300x300.jpg',
+        href: '/',
+    },
+    {
+        title: 'Bàn Ăn Thông Minh Mahal Kita Home LTX-801M.13.CTD',
+        img: 'HH-FURNITURE_N.LTX-801M.13.CTD_1900866816-300x300.jpg',
+        href: '/',
+    },
+];
+
+var doitacs = [
+    { alt: '', src: 'images/7a57bfce40ae81f0d8bf-300x300.jpg' },
+    { alt: '', src: 'images/31544eccb1ac70f229bd-300x300.jpg' },
+    { alt: '', src: 'images/ad4e55d6aab66be832a7-300x300.jpg' },
+    { alt: '', src: 'images/236583f37c93bdcde482-286x300.jpg' },
+    { alt: '', src: 'images/354728d2d7b216ec4fa3-300x300.jpg' },
+    { alt: '', src: 'images/95acff380058c1069849-300x300.jpg' },
+    { alt: '', src: 'images/a2ab573fa85f6901304e-300x300.jpg' },
+    { alt: '', src: 'images/c377a2e05d809cdec591-300x300.jpg' },
+    { alt: '', src: 'images/86711debe28b23d57a9a.jpg' },
+    { alt: '', src: 'images/40b42a2dd54d14134d5c-300x300.jpg' },
+];
+var posts = [
+    {
+        title: 'TOCEI Chia Sẻ Kinh Nghiệm Khi Mua Thiết Bị Vệ Sinh',
+        href: '',
+        img: 'images/2-1024x512.png',
+    },
+    {
+        title: 'TOCEI Chia Sẻ Kinh Nghiệm Khi Mua Thiết Bị Vệ Sinh',
+        href: '',
+        img: 'images/2-1024x512.png',
+    },
+    {
+        title: 'TOCEI Chia Sẻ Kinh Nghiệm Khi Mua Thiết Bị Vệ Sinh',
+        href: '',
+        img: 'images/2-1024x512.png',
+    },
+    {
+        title: 'TOCEI Chia Sẻ Kinh Nghiệm Khi Mua Thiết Bị Vệ Sinh',
+        href: '',
+        img: 'images/2-1024x512.png',
+    },
+    {
+        title: 'TOCEI Chia Sẻ Kinh Nghiệm Khi Mua Thiết Bị Vệ Sinh',
+        href: '',
+        img: 'images/2-1024x512.png',
+    },
+];
 router.get('/', (req, res, next) => {
-    res.render('pages/home/home', { layout: 'main', template: 'home-template', lsSubCat: data, lsCat });
+    res.render('pages/home/home', {
+        layout: 'main',
+        template: 'home-template',
+        lsSubCat: data,
+        lsCat,
+        lsProduct,
+        doitacs,
+        posts,
+    });
 });
 
 module.exports = router;
